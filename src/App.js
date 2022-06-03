de85977a198efdbc42d6431b7dfbc87052d11eb4
gabecolmenares <gabriel.colmenares@vk.ai>
2022-06-03 17:09:57 -0400

    First Ever Commit from VSCODE from Gabe

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>This is a test</h1>
+      <h1>This is a test Gabe can Access from VSCODE</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
